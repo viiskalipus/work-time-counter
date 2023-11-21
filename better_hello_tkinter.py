dffa3a8c1b6a91cde6243447341c148c90139520
Samuel Peiponen <pepponski@gmail.com>
2023-11-21 14:45:12 +0200

    Add variables and widgets in HelloView

--- better_hello_tkinter.py
+++ better_hello_tkinter.py
@@ -5,8 +5,31 @@ A better Hello World for Tkinter
 import tkinter as tk 
 from tkinter import ttk
 
+# Frame widget is typically used as a container for other widgets
+# Any number inside Frame class, all widgets inside is treated 
+# like a single widget
 class HelloView(tk.Frame):
     """A friendly little module"""
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        # StringVar is part of a collection of variable types in Tkinter
+        # that have special functionality, like automatic propagation of
+        # changes to widgets or event triggers. 
+        self.name = tk.StringVar()
+        self.hello_string = tk.StringVar()
+        self.hello_string.set("Hello World")
+        name_label = ttk.Label(self, text="Name: ")
+        name_entry = ttk.Entry(self, textvariable=self.name)
+        
+        ch_button = ttk.Button(self, text="Change", command=self.on_change)
+        hello_label = ttk.Label(self, textvariable=self.hello_string, 
+                                font=("Consolas", 64), wraplength=600)
+        
+        # Grid is a geometry manager, it allows to position widgets
+        # on their parent object by rows and columns. Sticky argument 
+        # takes cardinal direction N, S, E or W (tk.W or "W"). 
+        name_label.grid(row=0, column=0, sticky=tk.W)
+        name_entry.grid(row=0, column=1, sticky=(tk.W + tk.E))
+        ch_button.grid(row=0, column=2, sticky=tk.E)
+        hello_label.grid(row=1, column=0, columnspan=3)
         
