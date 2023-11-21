a9a769cf3decd33efc7d9cfae6b814df4b2b042e
Samuel Peiponen <pepponski@gmail.com>
2023-11-21 02:54:08 +0200

    Add frame

--- better_hello_tkinter.py
+++ better_hello_tkinter.py
@@ -1,6 +1,12 @@
 """   
 A better Hello World for Tkinter
 """
+# Avoid "star imports"
 import tkinter as tk 
 from tkinter import ttk
 
+class HelloView(tk.Frame):
+    """A friendly little module"""
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        
