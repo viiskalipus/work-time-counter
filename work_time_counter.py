7012227d7f4739c89241bc45f424be9fdac8f3cb
Samuel Peiponen <pepponski@gmail.com>
2023-12-12 19:36:30 +0200

    Time counter working as intended

--- work_time_counter.py
+++ work_time_counter.py
@@ -1,6 +1,7 @@
 """   
 Work Time Counter for keeping track on working time. 
 """
+# Imported libraries 
 import tkinter as tk 
 from tkinter import ttk
 import datetime
@@ -8,13 +9,18 @@ import time
 import tkinter.scrolledtext as scrolledtext
 from functools import partial
 
-
-
 # WorkTime Frame
 class WorkTime(tk.Frame):
     """Work Time counter main screen"""
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
+    def __init__(self, window=None):
+        super().__init__(window)
+        self.window = window
+        self.zero_time = ""
+        # Stopwatch running
+        self.is_running = False
+        self.total_h = 0
+        self.total_m = 0
+        self.total_s = 0
         
         # Set savename
         self.profile = tk.StringVar()
@@ -27,17 +33,15 @@ class WorkTime(tk.Frame):
                                 font=("Consolas", 12), wraplength=600)
         
         # Running time
-        self.time_elapsed = tk.StringVar()
-        self.time_elapsed.set("Elapsed time: ")
-        time_counter_label = ttk.Label(self, textvariable=self.time_elapsed, 
-                                font=("Consolas", 18), background="red")
+        self.elapsed_time = tk.StringVar()
+        self.elapsed_time_label = tk.Label(self, text="00:00:00", font=("Consolas", 24))
         
         # Current time
         self.current_time = tk.StringVar()
         self.current_time.set("Current time: " + 
                                 datetime.datetime.now().strftime("%H:%M:%S"))
         current_time_label = ttk.Label(self, textvariable=self.current_time, 
-                                font=("Consolas", 18), background="green")
+                                font=("Consolas", 24), background="yellow")
                
         # Set task
         self.task_name = tk.StringVar()
@@ -46,41 +50,35 @@ class WorkTime(tk.Frame):
         task_entry = ttk.Entry(self, textvariable=self.task_name)
         
         # Log
-        log_screen = scrolledtext.ScrolledText(self, height=8, width=100)
+        self.log_screen = scrolledtext.ScrolledText(self, height=8, width=100)
         log_label = ttk.Label(self, text="Log: ")
         
         # Buttons
         profile_ok_button = ttk.Button(self, text="OK", command=self.profile_on_change)
-        counter_start_button = ttk.Button(self, text="Start", 
-                                          command=partial(start_time, 
-                                                          log_screen, 
-                                                          self.task_name, 
-                                                          self.current_time))
-        counter_pause_button = ttk.Button(self, text="Pause", command=self.pause_time)
-        task_ok_button = ttk.Button(self, text="OK", 
-                                    command=partial(task_on_change,
-                                                    log_screen,
-                                                    self.task_name,
-                                                    self.current_time))
-        save_button = ttk.Button(self, text="Save", command=partial(save_to_file, 
-                                                                    self.profile,
-                                                                    log_screen))
+        task_ok_button = ttk.Button(self, text="OK", command=self.task_on_change)
+        save_button = ttk.Button(self, text="Save", command=self.save_to_file)
+        quit_button = ttk.Button(self, text="Quit", command=self.window.quit)
+        self.counter_start_button = ttk.Button(self, text="Start", command=self.start_time)
+        self.counter_pause_button = ttk.Button(self, text="Pause", command=self.pause_time)
+        self.counter_reset_button = ttk.Button(self, text="Reset", command=self.reset_time)
         
         # Grid positions
         profile_label.grid(row=0, column=0, sticky="W")
         name_entry.grid(row=0, column=1, sticky="WE")
         profile_ok_button.grid(row=0, column=2, sticky="E")
         save_label.grid(row=1, column=0, columnspan=3)
-        time_counter_label.grid(row=2, column=0, columnspan=2, rowspan=1, sticky="WSN")
+        self.elapsed_time_label.grid(row=2, column=0, columnspan=2, rowspan=1, sticky="WSN")
         current_time_label.grid(row=3, column=0, columnspan=2, rowspan=1, sticky="WSN")
-        counter_start_button.grid(row=2, column=2, sticky="E")
-        counter_pause_button.grid(row=3, column=2, sticky="E")
         task_label.grid(row=4, column=0, sticky="W")
         task_entry.grid(row=4, column=1, sticky="WE")
         task_ok_button.grid(row=4, column=2, sticky="E")
-        log_screen.grid(row=6, column=1, sticky="ES")
+        self.log_screen.grid(row=6, column=1, sticky="ES")
         log_label.grid(row=5, column=0, sticky="W")
         save_button.grid(row=7, column=2, sticky="ES")
+        quit_button.grid(row=8, column=2, sticky="ES")
+        self.counter_start_button.grid(row=1, column=2, sticky="E")
+        self.counter_pause_button.grid(row=2, column=2, sticky="E")
+        self.counter_reset_button.grid(row=3, column=2, sticky="E")
         
         # Column/row configurations
         self.columnconfigure(1, weight=1)
@@ -91,30 +89,48 @@ class WorkTime(tk.Frame):
         if snooze > 0:
             time.sleep(snooze)
         self.clock()
-        update_time()
-        
-        log_screen.insert("end", f"Date: [{datetime.date.today()}]\n")
-        
+        self.log_screen.insert("end", f"Date: [{datetime.date.today()}]\n")     
+
+    # Clock to keep current time
     def clock(self): 
         self.current_time.set(datetime.datetime.now().strftime("%H:%M:%S"))
         self.after(1000, self.clock)
     
     # When savefile profile is changed
     def profile_on_change(self):
-        # strip() strips out the whitespace in the variable. 
-        if self.profile.get().strip():
-            self.profile_string.set(f"Saved to file \"{self.profile.get()}.txt\"")
-        else:
-            self.profile_string.set("Saved to file \"unnamed.txt\"")
+        self.profile_string.set(f"Saved to file \"{self.profile.get()}.txt\"")
         
         # TODO
-        # - Add date to filename
+        # - Add date to filename        
+
+    # When time counter is started
+    def start_time(self):
+        # Update log
+        self.log_screen.insert("end", f"[{self.current_time.get()}]\[{self.elapsed_time.get()}]: START > Task [{self.task_name.get()}] started\n")
+        # Scroll to bottom after log is updated
+        self.log_screen.yview(tk.END)
+
+        # Check if can start
+        if not self.is_running:
+            self.elapsed_time_label.after(1000)
+            self.update_time()
+            self.is_running = True
+            
+        # TODO
+        # - If pause True and start False, start the timer
+        # - Disable start/continue button when time is not paused
+        # - Enable start/continue button when time is paused
 
     # When time counter is paused
     def pause_time(self):
-        pass
-    
-        # f"[{elapsed}]\n"
+        # Update log
+        self.log_screen.insert("end", f"[{self.current_time.get()}]\[{self.elapsed_time.get()}]: PAUSE > Task [{self.task_name.get()}] paused\n")
+        # Scroll to bottom after log is updated
+        self.log_screen.yview(tk.END)
+        
+        if self.is_running:
+            self.elapsed_time_label.after_cancel(self.zero_time)
+            self.is_running = False
         
         # TODO
         # If pause False but start True, pause the timer
@@ -122,58 +138,68 @@ class WorkTime(tk.Frame):
         # If pause True and start False, reset the timer
             # Change button to 'Pause'
         
-# Stop counting time
-def stop_time():
-    global is_running
-    is_running = False
+    # When time counter is reset
+    def reset_time(self):
+        # Update log
+        self.log_screen.insert("end", f"[{self.current_time.get()}]\[{self.elapsed_time.get()}]: RESET > Task [{self.task_name.get()}] reset\n")
+        # Scroll to bottom after log is updated
+        self.log_screen.yview(tk.END)
+        
+        if self.is_running:
+            self.counter_pause_button.after_cancel(self.zero_time)
+            self.is_running == False
+        self.total_h = 0
+        self.total_m = 0
+        self.total_s = 0
+        self.elapsed_time_label.config(text="00:00:00")
     
-# Update time
-def update_time():
-    if is_running:
-        elapsed_time = time.time() - t_start
-        time_elapsed.set(text="Elapsed time: " + str(elapsed_time))
-        time_elapsed.after(50, update_time)
+    # Update time counter
+    def update_time(self):
+        self.total_s += 1
+        if self.total_s == 60:
+            self.total_m += 1
+            self.total_s = 0
+        if self.total_m == 60:
+            self.total_h += 1
+            self.total_m = 0
+            
+        total_h_str = f"{self.total_h}" if self.total_h > 9 else f"0{self.total_h}"
+        total_m_str = f"{self.total_m}" if self.total_m > 9 else f"0{self.total_m}"
+        total_s_str = f"{self.total_s}" if self.total_s > 9 else f"0{self.total_s}"
+        
+        self.elapsed_time_label.config(text=
+                                    total_h_str + ":" +
+                                    total_m_str + ":" + 
+                                    total_s_str)
+        
+        self.elapsed_time.set(total_h_str + ":" +
+                                total_m_str + ":" + 
+                                total_s_str)
+        
+        self.zero_time = self.elapsed_time_label.after(1000, self.update_time)
 
-# Save log into a file
-def save_to_file(name, text):
-    filename = f"{name.get()}.txt"
-    file = open(filename, "w")
-    print(f"Saved to file {filename}: \n" + text.get("1.0", tk.END))
-    file.write(text.get("1.0", tk.END))
-    file.close()
-        
-    # When log updated, add to file
-    # filename: "profile.txt"
-    
-    # TODO
-    # - If application to be closed, stop timer and save to file before
-    # - Save to "profile_DD_MM_YY.txt"
+    # Save log into a file
+    def save_to_file(self):
+        filename = f"{self.profile.get()}.txt"
+        file = open(filename, "w")
+        print(f"Saved to file {filename}: \n" + self.log_screen.get("1.0", tk.END))
+        file.write(self.log_screen.get("1.0", tk.END))
+        file.close()
 
-# When current task is changed
-def task_on_change(destination, task, t):
-    # Update log
-    destination.insert('end', f'New task {task.get()} at [{t.get()}]\n')
-        
-# When time counter is started
-def start_time(destination, task, t):
-    # Update log
-    destination.insert("end", f"[{t.get()}]: Task {task.get()} started\n")
-    
-    # Initialize time counting
-    global is_running
-    global t_start
-    if not is_running: 
-        is_running = True
-        t_start = time.time()
-        update_time()
-    
-    # TODO
-    # - If pause True and start False, start the timer
-    # - Disable start/continue button when time is not paused
-    # - Enable start/continue button when time is paused
-        
-# Set timer running false by default
-is_running = False
+        # When log updated, add to file
+        # filename: "profile.txt"
+
+        # TODO
+        # - If application to be closed, stop timer and save to file before
+        # - Save to "profile_DD_MM_YY.txt"
+
+    # When current task is changed
+    def task_on_change(self):
+        # Update log
+        "[{self.current_time.get()}]\[{self.elapsed_time.get()}]: RESET > Task {self.task_name.get()} reset\n"
+        self.log_screen.insert("end", f"[{self.current_time.get()}]\[{self.elapsed_time.get()}]: TASK > New task [{self.task_name.get()}]\n")
+        # Scroll to bottom after log is updated
+        self.log_screen.yview(tk.END)
 
 class WorkTimeApplication(tk.Tk): 
     """Work Time Counter Main Application"""
@@ -183,7 +209,7 @@ class WorkTimeApplication(tk.Tk):
         # Window title
         self.title("Work Time Counter")
         # Sets the size of the window in pixels, default 600x300
-        self.geometry("600x350")
+        self.geometry("600x380")
         # Sets resizability of the window, default False
         self.resizable(width=False, height=False)
         
